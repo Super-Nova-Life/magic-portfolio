61a0dd7d71508a014d34a06a15aa00ddb8a69b43
Jay Clouds <theo@wezel.ltd>
2025-07-18 18:49:20 +0200

    Github OG update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # **Personal Portfolio Theo Wezel**
+
+![Personal Portfolio](https://wezel.ltd/images/og/home.jpg)
--- src/resources/content.js
+++ src/resources/content.js
@@ -91,9 +91,7 @@ const about = {
     title: "Introduction",
     description: (
       <>
-        Selene is a Jakarta-based design engineer with a passion for transforming complex challenges
-        into simple, elegant design solutions. Her work spans digital interfaces, interactive
-        experiences, and the convergence of design and technology.
+        Consectetur quis anim tempor dolor magna ea sunt exercitation ut. Fugiat enim excepteur nulla sunt. Nulla labore culpa mollit. Est proident amet anim labore sint. Enim aute veniam magna in cupidatat non esse amet enim Lorem laborum non aliquip consectetur officia.
       </>
     ),
   },
